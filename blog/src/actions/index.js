677ae91a111a96cf24ee51e76c8851edaffba2e5
MohamedArif20091999 <mohamedarifcbe@gmail.com>
2021-06-10 21:56:35 +0530

    "completed"

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,21 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+import _ from "lodash";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  console.log(getState().posts);
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach((id) => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async (dispatch) => {
   const { data } = await jsonPlaceholder.get("/posts");
 
   dispatch({ type: "FETCH_POSTS", payload: data });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: "FETCH_USER", payload: response.data });
 };
--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchPosts } from "../actions";
+import { fetchPosts, fetchPostsAndUsers, fetchUser } from "../actions";
 import "./postList.css";
 
 const App = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchPosts());
+    dispatch(fetchPostsAndUsers());
+    dispatch(fetchUser());
   }, []);
 
   const posts = useSelector((state) => state.posts);
+  const users = useSelector((state) => state.users);
+  console.log(users);
 
   const renderPosts = posts.map((post) => {
     return (
       <div className="ui cards one wide column">
-        <div key="post.title" class="card">
+        <div key="post.title" className="card">
           <div className="content">
-            <div className="header">User Name</div>
+            <div className="header">
+              {users.find((user) => post.userId == user.id).name}
+            </div>
             <div className="description">{post.body}</div>
           </div>
         </div>
